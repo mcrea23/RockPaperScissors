add524f08a18be2e0411c22b76d5845eaa2af999
+ <mcrea23@aol.com>
2021-03-11 09:39:08 -0600

    RPS-frontend: corrected spelling error

--- RPS-frontend/javascripts/index.js
+++ RPS-frontend/javascripts/index.js
@@ -1,15 +1,16 @@
 let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score"); /*Dom Variables Element span tag */
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_p = document.querySelector(".result");
-const rock_div = document.getElementById("rock");
-const paper_div = document.getElementById("paper");
-const scissors_div = document.getElementById("scissors");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+/*Dom Variables Element span tag */
 
 function getComputerChoice() {
-  const choices = ['rock','paper','scissors'];
+  const choices = ['r','p','s'];
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 }
@@ -32,35 +33,36 @@ function draw() {
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
-    case "rockscissors":
-    case "paperrock":
-    case "scissorspaper":
+    case "rs":
+    case "pr":
+    case "sp":
       win(userChoice, computerChoice);
       break;
-    case "rockpaper":
-    case "paperscissors":
-    case "scissorsrock":
+    case "rp":
+    case "ps":
+    case "sr":
       lose(userChoice, computerChoice);
       break;
-    case "rockrock":
-    case "paperpaper":
-    case "scissorsscissors":
+    case "rr":
+    case "pp":
+    case "ss":
       draw(userChoice, computerChoice);
       break;
   }
 }
 
 function main() {
+
   rock_div.addEventListener('click', function() {
-    game("rock");
+      game("r");
   })
 
   paper_div.addEventListener('click', function() {
-    game("paper");  
+      game("p");
   })
 
   scissors_div.addEventListener('click', function() {
-    game("scissors");
+      game("s");
   })
 }
 
