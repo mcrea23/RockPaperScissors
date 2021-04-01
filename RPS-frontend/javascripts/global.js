09ccf5b05a7ef4abaa333b4a0aa4aacfa8bbffd1
+ <mcrea23@aol.com>
2021-03-31 21:39:48 -0500

    RPS-frontend: moved variables to global

---
+++ RPS-frontend/javascripts/global.js
@@ -0,0 +1,25 @@
+const baseUrl = "http://localhost:3000"
+let userName = ""
+const nameInput = () => document.getElementById("name")
+const userScore_span = document.getElementById("user-score");
+const roboScore_span = document.getElementById("robo-score");
+let userScore = document.querySelector("userScore");
+let roboScore = document.querySelector("RoboScore");
+
+let choice = document.querySelector(".choice");
+ 
+let userResult = document.getElementById("userResult");
+let resultText = document.getElementById("resultText");
+let roboResult = document.getElementById("roboResult");
+
+const rock_div = document.getElementById("0");
+const paper_div = document.getElementById("1");
+const scissors_div = document.getElementById("2");
+
+const rockIcon = "<i class='fas fa-hand-rock fa-10x'></i>";
+const paperIcon = "<i class='fas fa-hand-paper fa-10x'></i>";
+const scissorIcon = "<i class='fas fa-hand-scissors fa-10x'></i>";
+
+const scoreBoard_div = document.querySelector(".score-board");
+const game_div = document.getElementById("game");
+const user_div = document.getElementById("user");
--- RPS-frontend/javascripts/index.js
+++ RPS-frontend/javascripts/index.js
@@ -1,29 +1,3 @@
-const baseUrl = "http://localhost:3000"
-let userName = ""
-const nameInput = () => document.getElementById("name")
-const userScore_span = document.getElementById("user-score");
-const roboScore_span = document.getElementById("robo-score");
-let userScore = document.querySelector("userScore");
-let roboScore = document.querySelector("RoboScore");
-
-let choice = document.querySelector(".choice");
- 
-let userResult = document.getElementById("userResult");
-let resultText = document.getElementById("resultText");
-let roboResult = document.getElementById("roboResult");
-
-const rock_div = document.getElementById("0");
-const paper_div = document.getElementById("1");
-const scissors_div = document.getElementById("2");
-
-const rockIcon = "<i class='fas fa-hand-rock fa-10x'></i>";
-const paperIcon = "<i class='fas fa-hand-paper fa-10x'></i>";
-const scissorIcon = "<i class='fas fa-hand-scissors fa-10x'></i>";
-
-const scoreBoard_div = document.querySelector(".score-board");
-const game_div = document.getElementById("game");
-const user_div = document.getElementById("user");
-
 
 window.addEventListener('DOMContentLoaded', (event) => {
     Game.renderForm()
@@ -45,7 +19,7 @@ function getRoboChoice() {
     roboScore_span.innerHTML = roboScore = (roboScore > 0) ? roboScore++ : 0;;
     userResult.innerHTML = convertToIcon(userChoice);
     roboResult.innerHTML = convertToIcon(roboChoice);
-    resultText.innerHTML = "win"
+    resultText.innerHTML = "WIN"
   }
   function lose(userChoice, roboChoice) {
     roboScore++;
@@ -53,7 +27,7 @@ function getRoboChoice() {
     roboScore_span.innerHTML = roboScore = (roboScore > 0) ? roboScore++ : 0;;
     userResult.innerHTML = convertToIcon(userChoice);
     roboResult.innerHTML = convertToIcon(roboChoice);
-    resultText.innerHTML = "loses";
+    resultText.innerHTML = "LOSES";
   }
   function draw(userChoice, roboChoice) {
     userResult.innerHTML = convertToIcon(userChoice);
